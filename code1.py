09a251b2a7904c365130f499661a3a34b6c481f3
johnrheypuyong <105416487+johnrheypuyong@users.noreply.github.com>
2022-05-29 16:08:47 +0800

    Update code1.py

--- code1.py
+++ code1.py
@@ -2,3 +2,17 @@ from cgitb import text
 import tkinter as tk
 from tkinter.font import BOLD
 from turtle import back
+
+class stopwatch(tk.Frame):
+
+    def __init__(self,window=None):
+        super().__init__(window)
+        self.window = window
+        self.new_time = ""
+        self.running = False
+        self.total_minutes = 0
+        self.total_seconds = 0
+        self.total_miliseconds = 0
+        self.pack
+        self.laps = 0
+        self.features()
